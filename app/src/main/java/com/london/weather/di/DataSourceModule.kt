6f2418e91c24b308048f48c35681a892ad79a856
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-06-09 00:40:16 +0300

    Configure HttpClient with JSON serialization and ignore unknown keys

--- app/src/main/java/com/london/weather/di/DataSourceModule.kt
+++ app/src/main/java/com/london/weather/di/DataSourceModule.kt
@@ -4,9 +4,18 @@ import com.london.weather.data.datasources.WeatherDataSource
 import com.london.weather.data.datasources.WeatherDataSourceImpl
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val dataSourceModule = module {
-    single { HttpClient(Android) }
+    single { HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true // Ignore unknown fields in the JSON response
+            })
+        }
+    } }
     single <WeatherDataSource> { WeatherDataSourceImpl(get()) }
 }
